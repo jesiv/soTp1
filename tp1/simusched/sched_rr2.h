1f4bec121bd1d3c2a0e19eb0850fa275830d9447
Jesica Vazquez <jevazquez@ws1.labo1>
2015-09-05 21:10:43 -0300

    round robin 2

--- tp1/simusched/sched_rr2.h
+++ tp1/simusched/sched_rr2.h
@@ -5,6 +5,8 @@
 #include <queue>
 #include "basesched.h"
 
+using namespace std;
+
 class SchedRR2 : public SchedBase {
 	public:
 		SchedRR2(std::vector<int> argn);
@@ -12,6 +14,25 @@ class SchedRR2 : public SchedBase {
 		virtual void load(int pid);
 		virtual void unblock(int pid);
 		virtual int tick(int cpu, const enum Motivo m);
+  private:
+ 
+    int proxIdDisponible(int);
+    int encontrarPos(int, int);
+    int finalizoQuantum(int);
+    int encontrarCPU(int);
+    int encontrarNucleo();
+
+    struct Cpu {
+      int quantum;
+      int pid;
+      int contador;
+      Cpu(int q): quantum(q), pid(0), contador(0) {};
+    };
+
+    vector<queue<int> > tareasACorrerPorCPU;
+    vector<vector<int> > tareasBloqueadas;
+    vector<Cpu> cores;
+
 };
 
 #endif
